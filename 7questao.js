26a785d20d551aca4f60b1efc8c143d9eb136a22
carisbe1777 <carisbe066@gmail.com>
2025-04-08 18:52:26 +0000

    finalizado

---
+++ 7questao.js
@@ -0,0 +1,11 @@
+function identificador (n1){
+    if (n1==0){
+        console.log("neutro")
+    }else if(n1%2==0){
+        console.log("par")
+    }else{
+        console.log ("impar")
+    }
+}
+identificador(32)
+identificador(0)
